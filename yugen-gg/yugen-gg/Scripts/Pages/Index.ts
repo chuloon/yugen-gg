082fb3f4694ab1896bd7e74750fbf3c0e16b5fc0
chuloon <ben.ng@outlook.com>
2016-11-20 09:10:05 -0500

    mod

--- yugen-gg/yugen-gg/Scripts/Pages/Index.ts
+++ yugen-gg/yugen-gg/Scripts/Pages/Index.ts
@@ -14,16 +14,19 @@ function indexViewModel() {
     self.emailText = ko.observable<string>("");
 
     this.mailingClick = () => {
+        debugger;
         let userId = self.emailText().replace(/[^a-zA-Z1-9 ]/g, "");
-         
         try {
-            if (self.emailText() != "") {
+            if (self.emailText() != "" && self.emailText().match("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?") != null) {
                 firebase.database().ref('/mailing-list/' + userId).set({
                     email: self.emailText()
                 });
 
                 self.emailText("");
                 alert("You've been successfully added to our mailing list!");
+            }
+            else {
+                alert("Invalid email. Please try again!")
             }
         }
         catch (ex) {
