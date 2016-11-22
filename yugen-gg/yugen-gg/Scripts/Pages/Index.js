8ff04240e02c7cd97ee31556f20322612684d2e7
chuloon <ben.ng@outlook.com>
2016-11-21 19:44:44 -0500

    implement about and events pages

--- yugen-gg/yugen-gg/Scripts/Pages/Index.js
+++ yugen-gg/yugen-gg/Scripts/Pages/Index.js
@@ -1,39 +1,31 @@
-﻿(<any>$("#email-textbox")).alpaca({
+$("#email-textbox").alpaca({
     "options": {
         "placeholder": "email address",
         "focus": false
     }
 });
-
 $("#alpaca1").attr("data-bind", "text: $data.emailText");
-
-declare var firebase: any;
-
 function indexViewModel() {
-    let self = this;
-
-    self.emailText = ko.observable<string>("");
-
-    this.mailingClick = () => {
+    var self = this;
+    self.emailText = ko.observable("");
+    this.mailingClick = function () {
         debugger;
-        let userId = self.emailText().replace(/[^a-zA-Z1-9 ]/g, "");
+        var userId = self.emailText().replace(/[^a-zA-Z1-9 ]/g, "");
         try {
             if (self.emailText() != "" && self.emailText().match("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?") != null) {
                 firebase.database().ref('/mailing-list/' + userId).set({
                     email: self.emailText()
                 });
-
                 self.emailText("");
                 alert("You've been successfully added to our mailing list!");
             }
             else {
-                alert("Invalid email. Please try again!")
+                alert("Invalid email. Please try again!");
             }
         }
         catch (ex) {
         }
-    }    
-
+    };
 }
-
 ko.applyBindings(new indexViewModel());
+//# sourceMappingURL=Index.js.map
